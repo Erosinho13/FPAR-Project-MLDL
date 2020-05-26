0b9972434751bc180221a5e01edca435859ffa39
ga1i13o <gabriele.trivigno@studenti.polito.it>
2020-05-26 22:56:03 +0200

    added logging to stages 1 to 3 and improved dataset cleaning function

--- clean_dataset.py
+++ clean_dataset.py
@@ -1,7 +1,21 @@
 import os
 TO_DELETE = ".DS_Store"
 
-def clean_data(directory):
+def clean_data(directory, depth=0):
+	elements = os.listdir(directory)
+	print(directory)
+	if TO_DELETE in elements:
+		print("removing ..  "+os.path.join(directory, elements[elements.index(TO_DELETE)]))
+		os.remove(os.path.join(directory, elements[elements.index(TO_DELETE)]))
+	if depth < 2:
+		for el in elements:
+			d = os.path.join(directory, el)
+			if os.path.isdir(d):
+				clean_data(d, depth+1)
+
+
+
+def clean_data_old(directory):
     print(directory)
     elements = os.listdir(directory)
     if TO_DELETE in elements:
