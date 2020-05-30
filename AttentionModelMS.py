20d633473043daa46790da0409e5982d6522e38c
rayquaza <eros.fani@gmail.com>
2020-05-30 17:38:16 +0200

    Bugfix

--- AttentionModelMS.py
+++ AttentionModelMS.py
@@ -33,59 +33,59 @@ class AttentionModelMS(nn.Module):
         
     def forward(self, inputVariable, no_cam = False, mmaps = False):
         
+        ms_out = []
+        
         state = (
             Variable(torch.zeros((inputVariable.size(1), self.mem_size, 7, 7))),
             Variable(torch.zeros((inputVariable.size(1), self.mem_size, 7, 7)))
         )
         
-        if not mmaps:
-        
-            if not no_cam:
+        if not no_cam:
 
-                for t in range(inputVariable.size(0)):
-
-                    logit, feature_conv, feature_convNBN = self.resNet(inputVariable[t])
-
-                    bz, nc, h, w = feature_conv.size()
-                    feature_conv1 = feature_conv.view(bz, nc, h*w)
-
-                    probs, idxs = logit.sort(1, True)
-                    class_idx = idxs[:, 0]
-
-                    cam = torch.bmm(self.weight_softmax[class_idx].unsqueeze(1), feature_conv1)
-
-                    attentionMAP = F.softmax(cam.squeeze(1), dim=1)
-                    attentionMAP = attentionMAP.view(attentionMAP.size(0), 1, 7, 7)
-
-                    attentionFeat = feature_convNBN * attentionMAP.expand_as(feature_conv)
-
-                    state = self.lstm_cell(attentionFeat, state)
-
-            else:
-
-                for t in range(inputVariable.size(0)):
-
-                    logit, feature_conv, feature_convNBN = self.resNet(inputVariable[t])
-
-                    state = self.lstm_cell(feature_convNBN, state)
-                
-            feats1 = self.avgpool(state[1]).view(state[1].size(0), -1)
-            feats = self.classifier(feats1)
-            
-            return feats, feats1
-        
-        else:
-            
-            ms_out = []
-            
             for t in range(inputVariable.size(0)):
+
+                logit, feature_conv, feature_convNBN = self.resNet(inputVariable[t])
                 
-                _, feature_conv, _ = self.resNet(inputVariable[t])
-                ms_out.append(self.msBlock(feature_conv))
-            
+                if mmaps:
+                    ms_out.append(self.msBlock(feature_conv))
+                
+                bz, nc, h, w = feature_conv.size()
+                feature_conv1 = feature_conv.view(bz, nc, h*w)
+
+                probs, idxs = logit.sort(1, True)
+                class_idx = idxs[:, 0]
+
+                cam = torch.bmm(self.weight_softmax[class_idx].unsqueeze(1), feature_conv1)
+
+                attentionMAP = F.softmax(cam.squeeze(1), dim=1)
+                attentionMAP = attentionMAP.view(attentionMAP.size(0), 1, 7, 7)
+
+                attentionFeat = feature_convNBN * attentionMAP.expand_as(feature_conv)
+
+                state = self.lstm_cell(attentionFeat, state)
+
+        else:
+
+            for t in range(inputVariable.size(0)):
+
+                logit, feature_conv, feature_convNBN = self.resNet(inputVariable[t])
+                
+                if mmaps:
+                    ms_out.append(self.msBlock(feature_conv))
+                
+                state = self.lstm_cell(feature_convNBN, state)
+
+        feats1 = self.avgpool(state[1]).view(state[1].size(0), -1)
+        feats = self.classifier(feats1)
+
+        if mmaps:
             ms_out = torch.stack(ms_out)
+            return feats, feats1, ms_out
+        
+        return feats, feats1
+        
             
-            return ms_out
+            
         
 
 def attention_model_ms(**kwargs):
